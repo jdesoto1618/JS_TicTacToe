9a3666e613223188b9c1ec67f67496c1abb85e66
Joe <modulusman@gmail.com>
2020-07-11 19:54:01 -0700

    Begins refactor of js code.
    Deletes AI function that was convoluting this
    program.
    
    Adds function to generate a board of 0's instead
    of hardcoding an array of 0's.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,49 +1,56 @@
-document.addEventListener('DOMContentLoaded', ()=> {
+function generateBoard(param) {
+  let board = [];
+  if(param) {
+    if(param == 9) {
+      let i = 0;
+      for (i; i < param; i++) {
+        board[i] = 0;
+      }
+    }
+  }
+  return board
+}
+
+let gameBoard = generateBoard(9);
+let counter = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
   $("td").on("click", function() {
     index = $(this).attr("id").split("")[1]; //grab the number of the ID only. Since I named my ID's f#, I need to grab the second index to get the number.
-    if(board[index] === 0) { //This works because it checks to see if an entry is 0 before proceeding. Index is defined above this with index = $(this).attr("id").split("")[1];.
+    if(gameBoard[index] === 0) { //This works because it checks to see if an entry is 0 before proceeding. Index is defined above this with index = $(this).attr("id").split("")[1];.
       if(counter % 2 === 0) {
-          $(this).text("X")
-          board[index] = player1;
+        $(this).text("X")
+        gameBoard[index] = player1;
+      } else {
+        $(this).text("O")
+        gameBoard[index] = player2;
       }
-  } //end of board index if statement
-    win(); //check who has won
+    }
+    win(); 
     counter++
-    AI();
+    console.log(gameBoard);
   })
+
   $("input").click(function(){
-    index = 0; //clear the index when the button is clicked. This is to start the game, or clear it to start it over.
-    board = [0,0,0,0,0,0,0,0,0];
+    index = 0;
+    gameBoard = generateBoard(9);
     counter = 0;
     $("td").text("")
     $("td").toggle("click")
     $("#player").text("")
     toggleReplayButton('off');
-  }) // end of td click
-}) //end of ready function
+  }) 
+}) 
+
 // save player names
 var p1Name = prompt('Player 1, enter your name!');
 var p2Name = prompt('Player 2, enter your name!');
 
-var board = [0,0,0,
-             0,0,0,
-             0,0,0]; //Use all zeros here as placeholders of sorts. Technically these dont have to be zero, but we went with zero when writing the rest of the code.
-var counter = 0; //This keeps track of which player's turn it is. It is used with mod 2 to switch between player1 and player2.
 var index; //Initialize this as empty. Once the program runs, each click will update index. The first click will update the index, which for now is on line 3.
 var player1 = 1;
 var player2 = -1; //This needs to be here since we used a check for each index being 0 in order to update the board. This doesn't have to be 2, but using 2 for player 2 sorta makes sense.
 var p1Score = 0;
 var p2Score = 0;
-
-function AI () {
-  index = Math.floor(Math.random()*9)
-  while (board[index] != 0) {
-    index = Math.floor(Math.random()*9) //if it's the wrong index, generate another number
-  }
-    board[index] = player2;
-    $("#f"+index).text("O")
-    counter++;
-}
 
 const toggleReplayButton = (toggle) => {
   if(toggle) {
@@ -58,27 +65,27 @@ const toggleReplayButton = (toggle) => {
   } 
 }
 
-function win() { //Can be refactored later, but these are set up to determine winners
-  if ((board[0] + board[1] + board[2] === 3 ||
-     (board[3] + board[4] + board[5]) === 3 ||
-     (board[6] + board[7] + board[8]) === 3 ||
-     (board[0] + board[3] + board[6]) === 3 ||
-     (board[1] + board[4] + board[7]) === 3 ||
-     (board[2] + board[5] + board[8]) === 3 ||
-     (board[0] + board[4] + board[8]) === 3 ||
-     (board[6] + board[4] + board[2]) === 3)) {
+function win() { 
+  if ((gameBoard[0] + gameBoard[1] + gameBoard[2] === 3 ||
+    (gameBoard[3] + gameBoard[4] + gameBoard[5]) === 3 ||
+    (gameBoard[6] + gameBoard[7] + gameBoard[8]) === 3 ||
+    (gameBoard[0] + gameBoard[3] + gameBoard[6]) === 3 ||
+    (gameBoard[1] + gameBoard[4] + gameBoard[7]) === 3 ||
+    (gameBoard[2] + gameBoard[5] + gameBoard[8]) === 3 ||
+    (gameBoard[0] + gameBoard[4] + gameBoard[8]) === 3 ||
+    (gameBoard[6] + gameBoard[4] + gameBoard[2]) === 3)) {
      $("#player").text(p1Name + " wins!");
     toggleReplayButton('on');
      $("td").toggle("click")
   }
-  else if ((board[0] + board[1] + board[2] === -3 ||
-     (board[3] + board[4] + board[5]) === -3 ||
-     (board[6] + board[7] + board[8]) === -3 ||
-     (board[0] + board[3] + board[6]) === -3 ||
-     (board[1] + board[4] + board[7]) === -3 ||
-     (board[2] + board[5] + board[8]) === -3 ||
-     (board[0] + board[4] + board[8]) === -3 ||
-     (board[6] + board[4] + board[2]) === -3)) {
+  else if ((gameBoard[0] + gameBoard[1] + gameBoard[2] === -3 ||
+    (gameBoard[3] + gameBoard[4] + gameBoard[5]) === -3 ||
+    (gameBoard[6] + gameBoard[7] + gameBoard[8]) === -3 ||
+    (gameBoard[0] + gameBoard[3] + gameBoard[6]) === -3 ||
+    (gameBoard[1] + gameBoard[4] + gameBoard[7]) === -3 ||
+    (gameBoard[2] + gameBoard[5] + gameBoard[8]) === -3 ||
+    (gameBoard[0] + gameBoard[4] + gameBoard[8]) === -3 ||
+    (gameBoard[6] + gameBoard[4] + gameBoard[2]) === -3)) {
     $("#player").text(p2Name + " wins!");
     toggleReplayButton('on');
     $("td").toggle("click")
